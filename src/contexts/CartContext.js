e9d983cc0d146916c861c72c3f8d6f9872c68530
Camsd <codecos.info@gmail.com>
2023-05-26 10:08:06 -0500

    plus and minus icon functions

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -37,7 +37,39 @@ const CartProvider = ({children}) => {
       setCart(newCart);
     }
 
-  return <CartContext.Provider value={{ cart, addToCart, removeFromCart }} >
+    // clear cart
+    const clearCart = ()=>{
+      setCart ([]);
+    };
+
+    //increase amount
+    const increaseAmount = (id) => {
+      const cartItem = cart.find((item) => item.id === id);
+      addToCart(cartItem, id);
+      console.log(`${id} amount increased`);
+    }
+
+    //decrease amount
+    const decreaseAmount = (id) => {
+      const cartItem = cart.find((item) => {
+        return item.id === id;
+      });
+      if (cartItem){
+        const newCart = cart.map(item => {
+          if(item.id === id){
+            return{...item, amount: cartItem.amount -1}
+          } else {
+            return item;
+          }
+        });
+        setCart(newCart);
+      }
+        if (cartItem.amount < 2){
+          removeFromCart(id);
+        }
+    }
+
+  return <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, increaseAmount, decreaseAmount }} >
     {children}
   </CartContext.Provider>;
 };
