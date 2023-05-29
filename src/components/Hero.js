7e70ac9c58a422f4e53cd5aca220631fbd5383cd
Camsd <codecos.info@gmail.com>
2023-05-29 17:32:27 -0500

    hero and sidebar Links

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -19,6 +19,9 @@ const Hero = () => {
           AUTUMN SALE STYLISH <br />
           <span className='font-semibold'>WOMENS</span>
         </h1>
+        <Link to={'/'} className='self-start uppercase font-semibold border-b-2 border-primary'>
+          Discover More
+        </Link>
       </div>
       {/* image  */}
       <div className='hidden lg:block'>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -19,7 +19,7 @@ const Sidebar = () => {
   return (
   <div className={`${
     isOpen ? 'right-0' : '-right-full'
-    } 'w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]'`}>
+    } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
 
     <div className='flex items-center justify-between py-6 border-b '>
       <div className='uppercase text-sm font-semibold'>Shopping Bag ({itemAmount})</div>
@@ -28,7 +28,7 @@ const Sidebar = () => {
         <IoMdArrowForward className='text-2xl' />
       </div>
     </div>
-    <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b'>
+    <div className='flex flex-col gap-y-2 h-[520px]  overflow-y-auto overflow-x-hidden border-b'>
       {cart.map((item) => {
       return <CartItem item={item} key={item.id} />
     })}
@@ -44,6 +44,8 @@ const Sidebar = () => {
             <FiTrash2 />
           </div>
         </div>
+        <Link to={'/'} className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium'>View cart</Link>
+        <Link to={'/'} className='bg-primary flex p-4 justify-center items-center text-white w-full font-medium'>Checkout</Link>
     </div>
   </div>
   );
