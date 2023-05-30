11ae78612632c1af82653e2dbca2d7ecfc8e1efa
Camsd <codecos.info@gmail.com>
2023-05-30 12:25:55 -0500

    product details

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -10,7 +10,7 @@ const ProductDetails = () => {
   // get the product id from the url
   const { id } = useParams()
   const { products } = useContext(ProductContext)
-  const { addToCart } = useContext(ProductContext)
+  const { addToCart } = useContext(CartContext)
 
   //get the sing product based on the id
   const product = products.find((item) => {
@@ -40,11 +40,12 @@ const ProductDetails = () => {
         </div>
         {/* text */}
         <div className='flex-1 text-center lg:text-left'>
-          <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto'>{title}</h1>
+          <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>{title}</h1>
           <div className='text-xl text-red-500 font-medium mb-6'>
            $ {price}
           </div>
           <p className='mb-8 '>{description}</p>
+          <button onClick={()=> addToCart(product, product.id)} className='bg-primary py-4 px-8 text-white'>Add to cart</button>
         </div>
       </div>
     </div>
